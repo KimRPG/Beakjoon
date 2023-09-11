2c42770617d06c438ece78fcc87835e2709cc7ce
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-09-11 22:06:39 +0900

    p1085 해결

---
+++ src/p1085/Main.java
@@ -0,0 +1,27 @@
+package p1085;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int w = s.nextInt();
+        int h = s.nextInt();
+
+        List distance = new ArrayList();
+
+        distance.add(x);
+        distance.add(y);
+        distance.add(w - x);
+        distance.add(h - y);
+
+        int minDistance = (Integer) Collections.min(distance);
+        System.out.println(minDistance);
+
+    }
+}
