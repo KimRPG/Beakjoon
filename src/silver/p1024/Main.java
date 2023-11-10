1483ced47b7f450287053a8841ff0d129819abb1
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-11-10 22:46:49 +0900

    수열의 합

--- src/silver/p1024/Main.java
+++ src/silver/p1024/Main.java
@@ -1,5 +1,7 @@
 package silver.p1024;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,14 +9,41 @@ public class Main {
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int L = s.nextInt();
-        if (L > 100) {
+        List<Integer> ans = new ArrayList<>();
 
+        for (int i = L; i <= 100; i++) {
+            if (i % 2 == 1) { // 홀수
+                if (N % i == 0) { // 깔끔하게 나누어지면
+                    int start = N / i - (i - 1) / 2;
+                    if (start >= 0) {
+                        for (int j = 0; j < i; j++) {
+                            ans.add(start + j);
+                        }
+                        break;
+                    }
+                }
+            } else { // 짝수
+                if (N % i == i / 2) { // 깔끔하게 나누어지면
+                    int start = N / i - i / 2 + 1;
+                    if (start >= 0) {
+                        for (int j = 0; j < i; j++) {
+                            ans.add(start + j);
+                        }
+                        break;
+                    }
+                }
+            }
         }
-    }
 
-    public static int line(int N, int L) {
-        if (L > 100) {
-            return -1;
+        if (ans.isEmpty()) {
+            ans.add(-1);
+        }
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i));
+            if (i < ans.size() - 1) {
+                System.out.print(" ");
+            }
         }
     }
 }
