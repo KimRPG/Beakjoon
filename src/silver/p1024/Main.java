437343d22eed207b97c9951563c20f2f447bbd2d
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-11-10 21:40:34 +0900

    .

---
+++ src/silver/p1024/Main.java
@@ -0,0 +1,20 @@
+package silver.p1024;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int L = s.nextInt();
+        if (L > 100) {
+
+        }
+    }
+
+    public static int line(int N, int L) {
+        if (L > 100) {
+            return -1;
+        }
+    }
+}
