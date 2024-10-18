aed7ff641a1354ba238db371912a59f7b002a96e
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-10-18 23:11:19 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -2,33 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Integer Macro(String st) {
-        return Integer.parseInt(st);
-    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Macro(st.nextToken());
-        int K = Macro(st.nextToken());
+        int N = Integer.parseInt((st.nextToken()));
+        int K = Integer.parseInt((st.nextToken()));
 
         int result = 0;
-        int a = 0;
+        int a = N-1;
         ArrayList list = new ArrayList();
         for (int i = 0; i < N; i++) {
-            list.add(Macro(br.readLine()));
+            list.add(Integer.parseInt(br.readLine()));
         }
-
-        list.sort(Comparator.reverseOrder());
 
         while (K != 0) {
             int add = K /(int) list.get(a);
             result += add;
             K -= (int) list.get(a) * add;
-            a++;
+            a--;
         }
         System.out.println(result);
     }
