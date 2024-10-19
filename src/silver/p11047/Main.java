7d989fd21d1fe4fd4b7ec80f2328a9ab5056dc1b
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-10-19 10:57:24 +0900

    p11047

---
+++ src/silver/p11047/Main.java
@@ -0,0 +1,32 @@
+package silver.p11047;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt((st.nextToken()));
+        int K = Integer.parseInt((st.nextToken()));
+
+        int result = 0;
+        int a = N-1;
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        while (K != 0) {
+            int add = K /(int) list.get(a);
+            result += add;
+            K -= (int) list.get(a) * add;
+            a--;
+        }
+        System.out.println(result);
+    }
+}
