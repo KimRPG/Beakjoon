0bff243b9d79c53f15855e24696df82d332787a5
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-10-28 13:56:12 +0900

    [Bronze V] Title: A+B - 4, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,13 +1,21 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            String s[] = sc.nextLine().split(" ");
-            System.out.println(Integer.parseInt(s[0]) +
-                    Integer.parseInt(s[1]));
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            String line = bf.readLine();
+            if (line == null || line.contentEquals("")) {
+                break;
+            }
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append((a + b)).append("\n");
         }
+        System.out.print(sb);
     }
 }
