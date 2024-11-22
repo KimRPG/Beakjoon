c42ba8b697d3a0a003eb04d021987e7c5122f2bc
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-11-22 15:12:48 +0900

    [Silver IV] Title: 수 찾기, Time: 532 ms, Memory: 54312 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,34 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            map.put(Integer.parseInt(st.nextToken()), 1);
         }
-        Collections.sort(list);
-        int M = Integer. parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            sb.append(isExist(list, 0, N-1, Integer.parseInt(st.nextToken()))).append("\n");
+            sb.append(map.getOrDefault(Integer.parseInt(st.nextToken()), 0)).append("\n");
         }
         System.out.println(sb);
+
     }
 
-    static int isExist(List<Integer> list, int low, int high, int key) {
-        if (high >= low) {
-            int mid = low + (high - low) / 2;
-
-            if (list.get(mid) == key) {
-                return 1;
-            } else if (list.get(mid) > key) {
-                return isExist(list, low, mid - 1, key);
-            } else {
-                return isExist(list, mid + 1, high, key);
-            }
-        }
-        return 0;
-    }
 }
