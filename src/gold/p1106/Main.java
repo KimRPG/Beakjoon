11ccb72d52843bffbe7d2bf7dc7f18d1be08eb08
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-11-23 10:41:00 +0000

    .

--- src/gold/p1106/Main.java
+++ src/gold/p1106/Main.java
@@ -7,29 +7,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-        int C = Integer.parseInt(st.nextToken()); // 최소 유치해야 할 고객 수
-        int N = Integer.parseInt(st.nextToken()); // 도시의 개수
-
-        int[] dp = new int[101]; // dp 배열 (최대 100,000명까지 처리)
-        Arrays.fill(dp, Integer.MAX_VALUE); // 초기값을 큰 값으로 설정
-        dp[0] = 0; // 고객을 0명 유치하기 위한 비용은 0
-
+        int[][] arr = new int[N][2];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int cost = Integer.parseInt(st.nextToken()); // 광고 비용
-            int customer = Integer.parseInt(st.nextToken()); // 해당 광고로 유치 가능한 고객 수
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
 
-            for (int j = customer; j < dp.length; j++) {
-                dp[j] = Math.min(dp[j], dp[j - customer] + cost);
+        // 한 번에 얻을 수 있는 고객의 수는 100보다 작거나 같다
+        // C명 이상이면 되므로 C + 101 까지 배열에 담는다
+        int[] dp = new int[C + 101];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < N; i++) {
+            int cost = arr[i][0];
+            int value = arr[i][1];
+            for (int j = value; j < C + 101; j++) {
+                if (dp[j - value] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j - value] + cost, dp[j]);
+                }
             }
         }
 
-        int result = Integer.MAX_VALUE;
-        for (int i = C; i < dp.length; i++) {
-            result = Math.min(result, dp[i]);
+        // C명 이상 중에 최솟값 탐색
+        int ans = Integer.MAX_VALUE;
+        for (int i = C; i < C + 101; i++) {
+            ans = Math.min(ans, dp[i]);
         }
 
-        System.out.println(result);
+        System.out.println(ans);
+
     }
+
 }
