a079e0698c20ed2c89e5db3b68dae68e6c85a92e
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-12-05 11:04:07 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 116 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println((int)Math.ceil((C - A) / (A - B * 1.0)) + 1);
+    }
+
+
+}
