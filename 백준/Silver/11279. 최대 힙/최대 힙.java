12dcef10e2e753140b778abd064fdd349755c408
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-12-16 09:55:19 +0900

    [Silver II] Title: 최대 힙, Time: 284 ms, Memory: 27356 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,22 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.PriorityQueue;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
-        for (int i = 0; i < N; i++) {
-            int a = Integer.parseInt(br.readLine());
-            if (a == 0 && pq.isEmpty()) {
-                System.out.println(0);
-            } else if (a == 0) {
-                System.out.println(pq.poll());
-            }else{
-                pq.add(a);
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            PriorityQueue<Integer> pq = new PriorityQueue<>((x, y) -> y - x);
+            for (int i = 0; i < N; i++) {
+                int a = Integer.parseInt(br.readLine());
+                if (a == 0 && pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else if (a == 0) {
+                    sb.append(pq.poll()).append("\n");
+                }else{
+                    pq.add(a);
+                }
             }
+            System.out.println(sb);
         }
     }
-}
