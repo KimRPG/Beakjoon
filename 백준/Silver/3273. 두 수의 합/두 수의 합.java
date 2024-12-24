4a36cc773723334b88b009228918f02a59bdeec5
KimRPG <120882082+KimRPG@users.noreply.github.com>
2024-12-24 16:46:25 +0900

    [Silver III] Title: 두 수의 합, Time: 312 ms, Memory: 33024 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
@@ -10,18 +9,24 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int ans = 0;
         HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> list = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] list = new int[N];
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            list[i] = Integer.parseInt(st.nextToken());
         }
+
         int a = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < N; i++) {
-            set.add(a - list.get(i));
+            int b = list[i];
+            if (set.contains(a - b)) {
+                ans++;
+            } else {
+                set.add(b);
+            }
         }
-        for (int i = 0; i < N; i++) {
-            if (set.contains(list.get(i))) ans++;
-        }
-        System.out.println(ans / 2);
+
+        System.out.println(ans);
     }
 }
