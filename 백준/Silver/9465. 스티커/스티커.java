35f45869fccf3886c33bd7dac7e6b10c92a38bfc
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-01-21 16:35:28 +0900

    [Silver I] Title: 스티커, Time: 716 ms, Memory: 108172 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -5,32 +5,33 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int[][] dp = new int[100_000][2];
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[][] arr = new int[n][2];
+
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[j][0] = Integer.parseInt(st.nextToken());
+                dp[j][0] = Integer.parseInt(st.nextToken());
             }
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[j][1] = Integer.parseInt(st.nextToken());
+                dp[j][1] = Integer.parseInt(st.nextToken());
             }
             if (n == 1) {
-                int ans = Math.max(arr[n - 1][0], arr[n - 1][1]);
+                int ans = Math.max(dp[n - 1][0], dp[n - 1][1]);
                 sb.append(ans).append("\n");
                 continue;
             }
-            arr[1][0] += arr[0][1];
-            arr[1][1] += arr[0][0];
+            dp[1][0] += dp[0][1];
+            dp[1][1] += dp[0][0];
 
             for (int j = 2; j < n; j++) {
-                arr[j][0] = Math.max(Math.max(arr[j - 1][1] + arr[j][0], arr[j - 2][0] + arr[j][0]), arr[j - 2][1] + arr[j][0]);
-                arr[j][1] = Math.max(Math.max(arr[j - 1][0] + arr[j][1], arr[j - 2][0] + arr[j][1]), arr[j - 2][1] + arr[j][1]);
+                dp[j][0] = Math.max(Math.max(dp[j - 1][1] + dp[j][0], dp[j - 2][0] + dp[j][0]), dp[j - 2][1] + dp[j][0]);
+                dp[j][1] = Math.max(Math.max(dp[j - 1][0] + dp[j][1], dp[j - 2][0] + dp[j][1]), dp[j - 2][1] + dp[j][1]);
             }
-            int ans = Math.max(arr[n - 1][0], arr[n - 1][1]);
+            int ans = Math.max(dp[n - 1][0], dp[n - 1][1]);
             sb.append(ans).append("\n");
         }
 
