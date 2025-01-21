3af8142c670f8b1eece46819c0f51e1fa3ed68fd
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-01-21 16:44:51 +0900

    [Silver I] Title: 스티커, Time: 240 ms, Memory: 16724 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,33 +1,29 @@
-import java.io.*;
-import java.util.*;
-
 class Main {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T, N, a, b, c;
-        StringTokenizer st;
 
-        T = Integer.parseInt(br.readLine());
+        T = read();
 
         int[][] dp = new int[2][100000];
         StringBuilder sb = new StringBuilder();
 
         while (T-- > 0) {
 
-            N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) dp[0][i] = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) dp[1][i] = Integer.parseInt(st.nextToken());;
+            N = read();
+
+            for (int i = 0; i < N; i++) dp[0][i] = read();
+            for (int i = 0; i < N; i++) dp[1][i] = read();
 
             if(N==1){
                 sb.append(Math.max(dp[0][N - 1], dp[1][N - 1])).append('\n');
                 continue;
             }
+
             dp[0][1] += dp[1][0];
             dp[1][1] += dp[0][0];
+
 
             for (int i = 2; i < N; i++) {
 
@@ -48,6 +44,15 @@ class Main {
 
     }
 
+    private static int read() throws Exception {
 
+        int c, n = System.in.read() & 15;
+
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+
+    }
 
 }
