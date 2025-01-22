c4af3f2c67d7c360adc179fcd1de082ece7f81c0
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-01-22 19:04:06 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 26888 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 class Main {
@@ -11,39 +13,41 @@ class Main {
         int[][] arr = new int[N][N];
         int[][] dp = new int[N][N];
 
-        // 삼각형 배열 입력
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= i; j++) { // `j <= i`로 변경
+            for (int j = 0; j < i + 1; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        // 초기값 설정
         dp[0][0] = arr[0][0];
         if (N == 1) {
             System.out.println(dp[0][0]);
             return;
         }
 
-        // DP 테이블 채우기
-        for (int i = 1; i < N; i++) {
+            dp[1][0] = arr[0][0] + arr[1][0];
+            dp[1][1] = arr[0][0] + arr[1][1];
+            if (N == 2) {
+            System.out.println();
+            System.out.println(Math.max(dp[1][0], dp[1][1]));
+            return;
+        }
+        for (int i = 2; i < N; i++) {
             for (int j = 0; j <= i; j++) {
                 if (j == 0) {
-                    dp[i][j] = dp[i - 1][j] + arr[i][j]; // 왼쪽 끝
+                    dp[i][j] = dp[i-1][j] + arr[i][j];
                 } else if (j == i) {
-                    dp[i][j] = dp[i - 1][j - 1] + arr[i][j]; // 오른쪽 끝
+                    dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j]; // 중간
+                    dp[i][j] = Math.max(dp[i - 1][j - 1] + arr[i][j], dp[i - 1][j] + arr[i][j]);
                 }
             }
         }
-
-        // 마지막 줄에서 최댓값 찾기
         int ans = 0;
         for (int i = 0; i < N; i++) {
-            ans = Math.max(ans, dp[N - 1][i]);
+            ans = Math.max(dp[N - 1][i], ans);
         }
         System.out.println(ans);
     }
+
 }
