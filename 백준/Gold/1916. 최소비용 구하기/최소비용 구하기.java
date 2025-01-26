9682c4341f4ede94ce249098f6f9477ac3eb6554
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-01-26 16:28:55 +0900

    [Gold V] Title: 최소비용 구하기, Time: 416 ms, Memory: 45720 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,68 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static List<Node>[] graph;
+    static int[][] graph;
     static int[] dist;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N + 1];
+        graph = new int[N + 1][N + 1];
         dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++) {
+            Arrays.fill(graph[i], -1);
         }
 
+        StringTokenizer st;
         for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph[start].add(new Node(end, cost));
+            graph[start][end] = graph[start][end] == -1 ? cost : Math.min(graph[start][end], cost);
         }
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        dijkstra(start);
-
-        System.out.println(dist[end]);
-    }
-
-    static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
-        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> {
+            return Integer.compare(o1.cost, o2.cost);
+        });
         pq.add(new Node(start, 0));
-
         while (!pq.isEmpty()) {
-            Node current = pq.poll();
-            int currentNode = current.node;
-            int currentCost = current.cost;
-
-            if (currentCost > dist[currentNode]) continue; // 이미 처리된 노드
-
-            for (Node next : graph[currentNode]) {
-                int newCost = dist[currentNode] + next.cost;
-
-                if (newCost < dist[next.node]) {
-                    dist[next.node] = newCost;
-                    pq.add(new Node(next.node, newCost));
+            Node node = pq.poll();
+            int point = node.node;
+            int cost = node.cost;
+            if (dist[point] < cost) continue;
+            dist[point] = cost;
+            for (int i = 1; i < N + 1; i++) {
+                if (graph[point][i] != -1) {
+                    int newCost = graph[point][i] + cost;
+                    if (dist[i] <= newCost) {
+                        continue;
+                    }
+                    pq.add(new Node(i, newCost));
                 }
             }
         }
+
+        System.out.print(dist[end]);
+
     }
 
-    static class Node {
-        int node, cost;
+    public static class Node {
+        int node;
+        int cost;
 
         Node(int node, int cost) {
             this.node = node;
             this.cost = cost;
         }
+
     }
 }
