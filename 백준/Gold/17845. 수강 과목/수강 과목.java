3cdcd03c0b44210cf6928b7e2f56a62c97b8e12a
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-02-14 16:35:06 +0900

    [Gold V] Title: 수강 과목, Time: 156 ms, Memory: 16824 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -1,46 +1,22 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int limit = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        Node[] nodes = new Node[K];
-        int[][] graph = new int[K+1][limit + 1];
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int value = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-            nodes[i] = new Node(value, time);
-        }
-
-        for (int i = 1; i <= K; i++) {
-            Node node = nodes[i - 1];
-            int value = node.value;
-            int time = node.time;
-            for (int j = 0; j <= limit; j++) {
-                if (time <= j) {
-                    graph[i][j] = Math.max(graph[i - 1][j], graph[i-1][j - time] + value);
-                }
-                else{
-                    graph[i][j] = graph[i - 1][j];
+    import java.io.*;
+    import java.util.*;
+    
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int limit = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int[] graph = new int[limit + 1];
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int value = Integer.parseInt(st.nextToken());
+                int time = Integer.parseInt(st.nextToken());
+                for (int j = limit; j >= time; j--) {
+                    graph[j] = Math.max(graph[j], graph[j - time] + value);
                 }
             }
+            System.out.println(graph[limit]);
         }
-
-        System.out.println(graph[K][limit]);
-
+    
     }
-
-    public static class Node{
-        int value;
-        int time;
-
-        public Node(int value, int time) {
-            this.value = value;
-            this.time = time;
-        }
-    }
-}
