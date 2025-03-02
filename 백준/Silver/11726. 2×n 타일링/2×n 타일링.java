08c5338c094880697f4628a0055cf16eba36a9a1
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-03-02 15:46:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[N];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+        }
+        System.out.println(dp[N - 1]);
+    }
+}
