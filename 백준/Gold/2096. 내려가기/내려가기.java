26abc629c0024016869d5bcdb80e8d2b65a21cb5
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-03-06 15:10:13 +0900

    [Gold V] Title: 내려가기, Time: 156 ms, Memory: 24424 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -3,15 +3,13 @@ import java.io.*;
 
 public class Main{
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        int N = read();
         int[][] maxDP = new int[N][3];
         int[][] minDP = new int[N][3];
-        int a = Integer.parseInt(st.nextToken());
-        int b= Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        int a = read();
+        int b = read();
+        int c = read();
         maxDP[0][0] = a;
         minDP[0][0] = a;
         maxDP[0][1] = b;
@@ -19,10 +17,9 @@ public class Main{
         maxDP[0][2] = c;
         minDP[0][2] = c;
         for (int i = 1; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b= Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
+            a = read();
+            b = read();
+            c = read();
             for (int j = 0; j < 3; j++) {
                 if (j == 0) {
                     maxDP[i][j] = Math.max(maxDP[i - 1][0], maxDP[i - 1][1]) + a;
@@ -39,5 +36,11 @@ public class Main{
         System.out.print(Math.max(Math.max(maxDP[N-1][0],maxDP[N-1][1]),maxDP[N-1][2]));
         System.out.print(" ");
         System.out.println(Math.min(Math.min(minDP[N-1][0],minDP[N-1][1]),minDP[N-1][2]));
+    }
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3 ) + (n << 1) + (c & 15);
+        return n;
     }
 }
