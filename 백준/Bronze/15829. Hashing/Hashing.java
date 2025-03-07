0fcd850ec8d0ba3165aa99a622bb82c2d6351819
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-03-07 15:04:42 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14316 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,21 +1,19 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-
+public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         char[] chars = br.readLine().toCharArray();
         long a = 0;
+        long mod = 1234567891;
+        long c = 1; // 31^i 값을 저장할 변수
+
         for (int i = 0; i < N; i++) {
-            long c = 1;
             int b = chars[i] - 'a' + 1;
-            for (int j = 0; j < i; j++) {
-                c *= 31;
-                c %= 1234567891;
-            }
-            a += b * c;
+            a = (a + (b * c) % mod) % mod; // (b * c)를 모듈러 연산 후 더하기
+            c = (c * 31) % mod; // c값을 31배 하면서 모듈러 연산 적용
         }
         System.out.println(a);
     }
