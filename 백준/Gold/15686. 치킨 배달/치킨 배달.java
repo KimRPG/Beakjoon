a233fa2307f9f62e69299ed13ede2cee3573563e
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-03-17 12:32:35 +0900

    [Gold V] Title: 치킨 배달, Time: 164 ms, Memory: 15528 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -2,13 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static int N, S;
-    static int ans = Integer.MAX_VALUE;  // 더 큰 값으로 초기화
+    
+    static int N,S;
+    static int ans = Integer.MAX_VALUE;
     static ArrayList<Point> home;
     static ArrayList<Point> chic;
     static Point[] ansChic;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,7 +16,6 @@ public class Main {
         home = new ArrayList<>();
         chic = new ArrayList<>();
         ansChic = new Point[S];
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -34,19 +32,18 @@ public class Main {
         System.out.println(ans);
     }
 
-    public static void dfs(int at, int depth) {
+    public static void dfs(int at,int depth) {
         if (depth == S) {
-            ans = Math.min(ans, dist(Arrays.asList(ansChic)));  // List로 변환하여 전달
+            ans = Math.min(ans, dist(ansChic));
             return;
         }
-
         for (int i = at; i < chic.size(); i++) {
             ansChic[depth] = chic.get(i);
             dfs(i + 1, depth + 1);
         }
     }
 
-    public static class Point {
+    public static class Point{
         int x, y;
 
         public Point(int x, int y) {
@@ -55,15 +52,16 @@ public class Main {
         }
     }
 
-    public static int dist(List<Point> selectedChickens) {
-        int totalDist = 0;
-        for (Point h : home) {
-            int minDist = Integer.MAX_VALUE;
-            for (Point c : selectedChickens) {
-                minDist = Math.min(minDist, Math.abs(h.x - c.x) + Math.abs(h.y - c.y));
+
+    public static int dist(Point[] chicken) {
+        int ans = 0;
+        for (Point point : home) {
+            int dis = Integer.MAX_VALUE;
+            for (Point chicken1 : chicken) {
+                dis = Math.min(dis, Math.abs(point.x - chicken1.x) + Math.abs(point.y - chicken1.y));
             }
-            totalDist += minDist;
+            ans += dis;
         }
-        return totalDist;
+        return ans;
     }
 }
