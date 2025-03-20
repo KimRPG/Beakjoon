4c768adacba7943d3f70d9ae193a6fb03e98ab6e
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-03-20 12:32:04 +0900

    [Gold V] Title: 합분해, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        long[] dp = new long[N + 1];
+        dp[0] = 1L;
+        for (int i = 0; i < K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[j] = (dp[j - 1] + dp[j]) % 1_000_000_000;
+            }
+        }
+        System.out.println(dp[N]);
+
+    }
+
+}
