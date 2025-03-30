e9a5a4260f856c5916a2d373221c6c41b52e8452
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-03-30 22:49:40 +0900

    [Gold III] Title: 앱, Time: 124 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -2,50 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int[] mem = new int[N];
+        int[] time = new int[N];
 
-        ArrayList<App> arrayList = new ArrayList<>();
-        int[][] apps = new int[N][2];
-        int[] dp = new int[M];
-        Arrays.fill(dp,100_000_000);
-        for (int i = 0; i < 2; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                apps[j][i] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            mem[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int totalCost = 0;
+        for (int i = 0; i < N; i++) totalCost += time[i];
+
+        int[] dp = new int[totalCost + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int c = totalCost; c >= time[i]; c--) {
+                dp[c] = Math.max(dp[c], dp[c - time[i]] + mem[i]);
             }
         }
-        for (int i = 0; i < N; i++) {
-            arrayList.add(new App(apps[i][0], apps[i][1]));
-        }
 
-        for (int i = 0; i < N; i++) {
-            App app = arrayList.get(i);
-            int mem = app.mem;
-            int time = app.time;
-            for (int j = M - 1; j >= 0; j--) {
-                if (j < mem) {
-                    dp[j] = Math.min(dp[j], time);
-                } else {
-                    dp[j] = Math.min(dp[j], dp[j - mem] + time);
-
-                }
+// 최소 비용 찾기
+        for (int c = 0; c <= totalCost; c++) {
+            if (dp[c] >= M) {
+                System.out.println(c);
+                break;
             }
-        }
-        System.out.println(dp[M - 1]);
-
-    }
-    public static class App{
-        int mem;
-        int time;
-
-        public App(int mem, int time) {
-            this.mem = mem;
-            this.time = time;
         }
     }
 }
