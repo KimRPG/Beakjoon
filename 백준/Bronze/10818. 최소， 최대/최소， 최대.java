6c26f2bea52b2851b979d9844c1ff2e91083e2ac
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-14 20:53:12 +0900

    [Bronze III] Title: 최소, 최대, Time: 276 ms, Memory: 14944 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,21 +1,36 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int max = -1_000_001;
+        int min = 1_000_001;
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            list.add(Integer.parseInt(st.nextToken()));
+            int a = read();
+            max = Math.max(max, a);
+            min = Math.min(min, a);
         }
-        int max = Collections.max(list);
-        int min = Collections.min(list);
-        System.out.print(min + " ");
-        System.out.println(max);
+        sb.append(min).append(" ").append(max);
+        System.out.println(sb);
+    }
+    private static int read() throws Exception {
+        int c = System.in.read();
+        int n = 0;
+        boolean negative = false;
+
+        // 음수 처리
+        if (c == '-') {
+            negative = true;
+            c = System.in.read();
+        }
+
+        n = c & 15;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        return negative ? -n : n;
     }
 }
