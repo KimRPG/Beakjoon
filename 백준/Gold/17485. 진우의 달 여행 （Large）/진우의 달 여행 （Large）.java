c6e950b3ce81fc49d78dfe334697cb035d0d8c2b
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-19 14:24:42 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 288 ms, Memory: 55464 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.java
@@ -4,23 +4,20 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = read();
+        int M = read();
         int[][] arr = new int[N][M];
         int[][][] dp = new int[N][M][3];
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            int a = Integer.parseInt(st.nextToken());
+            int a = read();
             arr[0][i] = a;
             dp[0][i][0] = a;
             dp[0][i][1] = a;
             dp[0][i][2] = a;
         }
         for (int i = 1; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = read();
             }
         }
 
@@ -52,5 +49,15 @@ public class Main {
         System.out.println(min);
     }
 
+    private static int read() throws Exception {
+
+        int c, n = System.in.read() & 15;
+
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+
+    }
 
 }
