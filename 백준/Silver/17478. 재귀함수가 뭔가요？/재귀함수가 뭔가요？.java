bb5dbb6009b6a1be74a5a48a65210ab2ad9b54db
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-23 14:49:54 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 108 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -14,23 +14,18 @@ public class Main {
     }
 
     public static void func(int n) {
-        sb.append("____".repeat(Math.max(0, N - n)));
-        sb.append("\"재귀함수가 뭔가요?\"").append("\n");
+        StringBuilder prefix = new StringBuilder();
+        prefix.append("____".repeat(N - n));
+        sb.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
         if (n == 0) {
-            sb.append("____".repeat(Math.max(0, N - n)));
-            sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
-            sb.append("____".repeat(Math.max(0, N - n)));
-            sb.append("라고 답변하였지.").append("\n");
+            sb.append(prefix).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+            sb.append(prefix).append("라고 답변하였지.").append("\n");
             return;
         }
-        sb.append("____".repeat(Math.max(0, N - n)));
-        sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
-        sb.append("____".repeat(Math.max(0, N - n)));
-        sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
-        sb.append("____".repeat(Math.max(0, N - n)));
-        sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+        sb.append(prefix).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+        sb.append(prefix).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
+        sb.append(prefix).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
         func(n - 1);
-        sb.append("____".repeat(Math.max(0, N - n)));
-        sb.append("라고 답변하였지.").append("\n");
+        sb.append(prefix).append("라고 답변하였지.").append("\n");
     }
 }
