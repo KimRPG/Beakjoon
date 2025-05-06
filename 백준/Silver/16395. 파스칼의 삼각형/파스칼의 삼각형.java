0e9601b7f210f5aee75e8453738047bb3798aab7
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-05-06 21:35:59 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -3,19 +3,18 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long[][] dp = new long[30][30];
-        dp[0][0] = 1;
-        for (int i = 1; i < 30; i++) {
-            dp[i][0] = 1;
-            dp[i][i] = 1;
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()) - 1;
+        int k = Integer.parseInt(st.nextToken()) - 1;
+        if (k > n - k) k = n - k;  // 대칭성 이용
+        long result = 1;
+
+        for (int i = 1; i <= k; i++) {
+            result *= (n - i + 1);
+            result /= i;
         }
-        String[] st = br.readLine().split(" ");
-        int N = Integer.parseInt(st[0]);
-        int M = Integer.parseInt(st[1]);
-        System.out.println(dp[N - 1][M - 1]);
+
+        System.out.println(result);
 
     }
 }
