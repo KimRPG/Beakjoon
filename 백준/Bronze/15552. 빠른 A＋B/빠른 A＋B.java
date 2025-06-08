d3b6c77d0b97ba28239ba3bd23cf88a4cbfdb9b4
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-06-08 23:02:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 760 ms, Memory: 221992 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
+
