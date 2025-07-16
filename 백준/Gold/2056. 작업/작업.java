2aa3bedd96ad8af709b37354cbfea70e660d2306
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-07-16 23:18:12 +0900

    [Gold IV] Title: 작업, Time: 416 ms, Memory: 52864 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.java
+++ 백준/Gold/2056. 작업/작업.java
@@ -6,46 +6,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] inDegree = new int[N];
         int[] times = new int[N];
         int[] result = new int[N];
-        List<Integer>[] arr = new ArrayList[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = new ArrayList<>();
-        }
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            times[i] =  Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
-            inDegree[i] = num;
-            for (int j = 0; j < num; j++) {
-                int to = Integer.parseInt(st.nextToken());
-                arr[to-1].add(i);
-            }
-        }
-        Queue<Integer> queue = new ArrayDeque<>();
-        for (int i = 0; i < N; i++) {
-            if (inDegree[i] == 0) {
-                queue.add(i);
-                result[i] = times[i];
-            }
-        }
-        while (!queue.isEmpty()) {
-            int vertices = queue.poll();
-            for (int a : arr[vertices]) {
-                inDegree[a]--;
-                result[a] = Math.max(result[a], result[vertices] + times[a]);
-                if (inDegree[a] == 0) {
-                    queue.add(a);
-                }
+            int time = Integer.parseInt(st.nextToken());
+            times[i] = time;
+            result[i] = time;
+            int a = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < a; j++) {
+                result[i] = Math.max(result[i], result[Integer.parseInt(st.nextToken()) - 1] + times[i]);
             }
         }
         int max = 0;
-        for (int i = 0; i < N; i++) {
-            max = Math.max(max, result[i]);
+        for (int a : result) {
+            max = Math.max(a, max);
         }
 
         System.out.println(max);
-
     }
 }
