1ac6df1318cef7bab1f4365e43961da487a30b17
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-09-02 21:13:02 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+            string[] st = Console.ReadLine().Split();
+            int a = int.Parse(st[0]);
+            int b = int.Parse(st[1]);
+            Console.WriteLine(a+b);
+        }
+    }
+}
