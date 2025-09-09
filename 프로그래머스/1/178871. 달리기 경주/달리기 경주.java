8c61b30efcfc3d62cb011d5d8a8cc1a3d0bd31c9
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-09-09 21:41:47 +0900

    [level 1] Title: 달리기 경주, Time: 931.56 ms, Memory: 356 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -5,6 +5,8 @@ class Solution {
     public String[] solution(String[] players, String[] callings) {
         answer = players;
         map = new HashMap<>(); 
+        
+        
         for(int i = 0; i < players.length ; i++){
             map.put(players[i], i);
         }
@@ -13,13 +15,14 @@ class Solution {
         }
         return answer;
     }
+    
     public void change(String player){
         int place = map.get(player);
         String player2 = answer[place - 1];
         answer[place - 1] = player;
         answer[place] = player2;
         
-        map.put(player, place-1);
+        map.put(player, place - 1);
         map.put(player2, place);
     }
 }
