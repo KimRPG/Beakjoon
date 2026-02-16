22ad9f7c505ab970da34e95e908d0ea46597bd60
KimRPG <120882082+KimRPG@users.noreply.github.com>
2026-02-16 14:05:05 +0900

    [Silver II] Title: 상자넣기, Time: 128 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+import java.sql.SQLOutput;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int answer = 0;
+        for (int ans : dp) {
+            answer = Math.max(answer, ans);
+        }
+        System.out.println(answer + 1);
+
+    }
+}
