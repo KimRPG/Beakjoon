81436a7a2c2895d35841715fc056d2d9ee40c3f9
KimRPG <120882082+KimRPG@users.noreply.github.com>
2026-02-16 14:09:29 +0900

    [Silver II] Title: 상자넣기, Time: 112 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.java
+++ 백준/Silver/1965. 상자넣기/상자넣기.java
@@ -1,5 +1,3 @@
-import java.math.BigInteger;
-import java.sql.SQLOutput;
 import java.util.*;
 import java.io.*;
 
@@ -8,23 +6,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int[] dp = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+
+        List<Integer> lis = new ArrayList<>();
+
+        for (int x : arr) {
+            if (lis.size() == 0 || lis.get(lis.size() - 1) < x) {
+                lis.add(x);
+            } else {
+                int pos = binarySearch(lis, x);
+                lis.set(pos, x);
             }
         }
-        int answer = 0;
-        for (int ans : dp) {
-            answer = Math.max(answer, ans);
-        }
-        System.out.println(answer + 1);
 
+        System.out.println(lis.size());
+    }
+
+    private static int binarySearch(List<Integer> list, int target) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (list.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low; 
     }
 }
