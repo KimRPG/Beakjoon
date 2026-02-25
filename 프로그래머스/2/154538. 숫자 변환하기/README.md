4dfc079cecdee61997ec6f10ee7b40310d33556f
KimRPG <120882082+KimRPG@users.noreply.github.com>
2026-02-25 11:38:30 +0900

    [level 2] Title: 숫자 변환하기, Time: 34.23 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/README.md
+++ 프로그래머스/2/154538. 숫자 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 변환하기 - 154538 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538#) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 81.05 ms
+메모리: 100 MB, 시간: 34.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 25일 11:33:10
+2026년 02월 25일 11:38:26
 
 ### 문제 설명
 
--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,37 +1,18 @@
 import java.util.*;
 
 class Solution {
+
+    int[] dp = new int[3000003];
+    int INF = 1000002;
+
     public int solution(int x, int y, int n) {
-        int answer = -1;
-        Queue<int[]> queue = new ArrayDeque<>();
-        boolean[] isVisit = new boolean[y+1];
-        queue.add(new int[]{x,0});
-        isVisit[x] = true;
-        while(!queue.isEmpty()){
-            int[] cur = queue.poll();
-            if(cur[0] == y){
-                answer = cur[1];
-                break;
-            }
-            if(cur[0] + n <= y){
-                if(!isVisit[cur[0]+n]){
-                    queue.add(new int[]{cur[0]+n, cur[1]+1});
-                    isVisit[cur[0]+n] = true;
-                }
-            }
-            if(cur[0] * 2 <= y){
-                if(!isVisit[cur[0]*2]){
-                    queue.add(new int[]{cur[0]*2, cur[1]+1});
-                    isVisit[cur[0]*2] = true;
-                }
-            }
-            if(cur[0] * 3 <= y){
-                if(!isVisit[cur[0]*3]){
-                    queue.add(new int[]{cur[0]*3, cur[1]+1});
-                    isVisit[cur[0]*3] = true;
-                }
-            }
+        int answer = 0;
+        Arrays.fill(dp, INF);
+        dp[x] = -1;
+        dp[y] = 0;
+        for(int num = Math.max(y - n, Math.max(y / 2, y / 3)); num >= x; num--){
+            dp[num] = Math.min(dp[num + n] + 1, Math.min(dp[num * 2] + 1, dp[num * 3] + 1));
         }
-        return answer;
+        return dp[x] >= INF ? -1 : dp[x];
     }
 }
